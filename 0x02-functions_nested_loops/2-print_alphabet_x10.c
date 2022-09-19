59655dfcb96640c692dfd179358bcd1aabe311b6
Dae <jdarahthomas@gmail.com>
2022-09-19 16:06:23 -0700

    d

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,11 @@
  * print_alphabet_x10 - print lowercase alphabet ten times
  * Return: 0
  */
-void 'print_alphabet_x10'(void)
+void print_alphabet_x10(void)
 {
 	char alpha;
 	int counter;
-	for (counter = 1; counter <= 10; counter++);
+	for (counter =  1 counter <= 10 counter++);
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++);
 		{
