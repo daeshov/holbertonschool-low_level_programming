99eea80c0ae71f798c614d9ca9f75d501d0b2741
Dae <jdarahthomas@gmail.com>
2022-09-19 15:20:02 -0700

    d

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@ void 'print_alphabet_x10'(void)
 {
 	char alpha;
 	int counter;
-	for (counter = 1; counter <= 10; counter++)
+	for (counter = 1; counter <= 10; counter++);
 	{
-		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++);
 		{
 			_putchar(alpha);
 		}
