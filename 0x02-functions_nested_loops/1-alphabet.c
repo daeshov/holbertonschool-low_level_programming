af217a7be97b38bc8d98075bf5e268c30d786b74
Dae <jdarahthomas@gmail.com>
2022-09-19 12:12:47 -0700

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * print_alphabet - prints lowercase alphabet
  * Return: 0
  */
+
 void print_alphabet(void)
 {
 	char alpha;
+
 	alpha = 'a';
+
 	while (alpha <= 'z')
 	{
 		_putchar(alpha);
