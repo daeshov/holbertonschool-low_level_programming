3dd36ae3ae1d3d12458c14578cf6c8efa79db313
Dae <jdarahthomas@gmail.com>
2022-09-20 09:33:05 -0700

    s

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,8 +3,8 @@
 /**
  * print_last_digit - print last digit of integer
  * @n: integer argument
- * Return: last digit of number 
- */ 
+ * Return: last digit of number
+ */
 
 int print_last_digit(int n)
 {
