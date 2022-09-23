6e248d23f6b2b9fa9eb1773348da7fa3464954af
Dae <jdarahthomas@gmail.com>
2022-09-23 08:47:53 -0700

    s

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,8 @@
 
 /**
 * _isupper - Write a function that checks for uppercase character,
-* return: always 1 if capitol 
+* return: always 1 if capitol.
 */
-
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
@@ -12,6 +11,5 @@ if (c >= 65 && c <= 90)
 	return (1);
 }
 else
-{
 return (0);
 }
