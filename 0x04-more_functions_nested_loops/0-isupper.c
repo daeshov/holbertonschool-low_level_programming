a88ce76fb54d8f3251a88bc8bfb550778b7d97d9
Dae <jdarahthomas@gmail.com>
2022-09-23 07:44:46 -0700

    s

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *Write a function that checks for uppercase character, returns one if c *if c is uppercase zero for otherwise 
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c<= 'Z')
+		return (1)
+
+	return (0);
+}
