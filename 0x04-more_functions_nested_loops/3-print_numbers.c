5163cb17b9a3702b97388d1e843b883bebed79ed
Dae <jdarahthomas@gmail.com>
2022-09-24 10:34:12 -0700

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * print_numbers - print 0 to 9
+ * use _putchar twice
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar('i');
+
+	_putchar('\n');
+}
