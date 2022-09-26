8493ac98e7effa413eb6154c9be2455020518c1b
Dae <jdarahthomas@gmail.com>
2022-09-26 07:24:21 -0700

    dfgj

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 
 /**
  * print_numbers - print 0 to 9, followed by new line
@@ -8,10 +7,10 @@
 
 void print_numbers(void)
 {
-	char i;
-
-	for (i = '0'; i <= '9'; i++)
-		_putchar(i);
-
-	_putchar('\n');
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
