d2d2d7b15890ea42b36ceece2675db075e5e2a08
Dae <jdarahthomas@gmail.com>
2022-09-26 11:58:30 -0700

    dd

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include "_putchar.c"
 
 /**
  * print_line - print the character "_" n times to draw a line
  * @n: number of characters to draw
+ * only use _putchar function to print
  */
 
 void print_line(int n)
