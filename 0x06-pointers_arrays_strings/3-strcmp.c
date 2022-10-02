5ee8c96fccc601cde672c004283cfd33b2452f00
Dae <jdarahthomas@gmail.com>
2022-10-02 11:08:33 -0700

    ee

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_strcmp - compares two strings
+ *@s1: pointer to first string
+ *@s2: pointer to second string
+ *Return: always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+int counter, cmpVal;
+counter = 0;
+while (s1[counter] == s2[counter] && s1[counter] != '\0')
+{
+	counter++;
+}
+cmpVal = s1[counter] - s2[counter];
+return (cmpVal);
+}
