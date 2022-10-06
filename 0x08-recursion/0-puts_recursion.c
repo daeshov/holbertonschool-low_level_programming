23b989612b9d4701673f54b2100e4b07efdaf6bd
Dae <jdarahthomas@gmail.com>
2022-10-06 14:20:36 -0700

    ff

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *Return: return pointer to string
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s == '\0');
+	_putchar('\n');
+	return;
+
+}
