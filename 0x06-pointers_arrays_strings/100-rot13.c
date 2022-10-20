d1d4144671b2c2ef551d0f0abea3508b6a40a671
Dae <jdarahthomas@gmail.com>
2022-10-20 12:42:46 -0700

    ff

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include"holberton.h"
+/**
+ * rot13 - function that encodes a string
+ * Return: always 0
+ */
+
+char *rot13(char *)
+{
+
+int m;
+char encode[6] = {'r', 'o', 't', '1', '3', '\0'};
+for (m = 0; m < encode; m++)
+return (0);
+}
