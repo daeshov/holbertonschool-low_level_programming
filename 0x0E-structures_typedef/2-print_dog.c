854a2036e7c59b23563bddb26a38902c82827042
Dae <jdarahthomas@gmail.com>
2022-10-21 10:17:35 -0700

    dd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 
 	printf("Age: %f\n", d->age);
 
-	(d->owner == NULL) ? printf("owner: (nil)\n")
-		: printf("owner: %s\n", d->owner);
+	(d->owner == NULL) ? printf("Owner: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
 
 }
