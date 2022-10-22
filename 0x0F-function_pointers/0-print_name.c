167bfcf4bbe47474709d1ff1884bc555d95d8c3c
Dae <jdarahthomas@gmail.com>
2022-10-22 11:49:20 -0700

    dd

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - execute function given
+ * @name: parameter that function ptr needs
+ * @f: function to execute
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+	f(name);
+}
