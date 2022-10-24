3643a3b3e36dd3f6c260d9310adb3bf8b45a53a5
Dae <jdarahthomas@gmail.com>
2022-10-24 14:16:23 -0700

    ss

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - function that prints numbers followed by new line
+ *@separator
+ *
+ *
+ *
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int num;
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
+#include <stdarg.h>
+#include <stdlib.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
