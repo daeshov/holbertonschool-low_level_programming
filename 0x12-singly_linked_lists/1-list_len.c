29b4cd77b0d2d88685bbe3c8f7fad59039537ce4
Dae <jdarahthomas@gmail.com>
2022-11-04 10:59:06 -0700

    gg

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements linked
+ *@h: pointer to
+ *Return: number of elements
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+
+
+	return (num);
+}
