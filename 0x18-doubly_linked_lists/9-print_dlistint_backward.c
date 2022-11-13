ff6387b38cbeac2974f6e62456ed16ad67045ddd
Dae <jdarahthomas@gmail.com>
2022-11-13 14:54:10 -0800

    dd

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * size_t print_dlistint_backward - prints dlistint_t list backward
+ *print_dlistint_backward - prints dlistint_t list backward
  *@h: pointer of head of list
  *
  *Return: number of nodes
@@ -10,15 +10,16 @@
 size_t print_dlistint_backward(const dlistint_t *h)
 {
 	dlistint_t *end, *n = malloc(sizeof(dlistint_t));
-	
+
 	while (end != NULL)
 	{
 		end = n->prev;
 		n->prev = h->next;
 		n->next = h->prev;
 		end = n->next;
-		printf("%d\n", end->next);
+		end++;
+		printf("\n[h] <=>");
 	}
-	
-	return (n);
+
+	return (h->n);
 }
