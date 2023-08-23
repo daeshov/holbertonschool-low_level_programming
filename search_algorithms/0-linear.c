05a742fe5efba750518739901f99056ef24b7f76
daeshov <jdarahthomas@gmail.com>
2023-08-23 15:59:04 -0500

    fix 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,7 +3,7 @@
 
 int linear_search(int *array, size_t size, int value) {
     size_t i;
-    
+
     if (array == NULL)
         return -1;
 
@@ -16,16 +16,3 @@ int linear_search(int *array, size_t size, int value) {
     return -1; /* Value not found */
 }
 
-int main(void) {
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    size_t size = sizeof(array) / sizeof(array[0]);
-    int value = 5;
-    int index = linear_search(array, size, value);
-
-    if (index != -1)
-        printf("Value %d found at index %d\n", value, index);
-    else
-        printf("Value %d not found\n", value);
-
-    return 0;
-}
