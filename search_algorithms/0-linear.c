39a11e0649bd6afae88d21eb6d9b1ae2e81fbfdd
daeshov <jdarahthomas@gmail.com>
2023-08-23 16:06:03 -0500

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
 #include <stddef.h>
 
-int binary_search(int *array, size_t size, int value) {
-    int low = 0;
-    int high = size - 1;
-    
-    if (array == NULL)
+int linear_search(int *array, size_t size, int value) {
+    size_t i;
 
+    if (array == NULL)
         return -1;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        printf("Searching in array: ");
-        for (size_t i = low; i <= (size_t)high; i++) {
-            printf("[%d]", array[i]);
-            if (i != (size_t)high)
-                printf(", ");
-        }
-        printf("\n");
-
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
+    for (i = 0; i < size; i++) {
+        printf("Value checked array[%lu] = %d\n", i, array[i]);
+        if (array[i] == value)
+            return (int)i;
     }
 
     return -1; /* Value not found */
