2b80f13e0d27da8d11dd887d14f97667583225ee
daeshov <jdarahthomas@gmail.com>
2023-08-23 16:33:15 -0500

    task 2

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -2,29 +2,28 @@
 #include <stddef.h>
 
 int binary_search(int *array, size_t size, int value) {
-    if (array == NULL)
-        return -1;
+int low = 0;
+int high = size - 1;
 
-    int low = 0;
-    int high = size - 1;
+if (array == NULL)
+return -1;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        printf("Searching in array: ");
-        for (size_t i = low; i <= (size_t)high; i++) {
-            printf("%d", array[i]);
-            if (i != (size_t)high)
-                printf(", ");
-        }
-        printf("\n");
+while (low <= high) {
+int mid = (low + high) / 2;
+printf("Searching in array: ");
+for (size_t i = low; i <= (size_t)high; i++) {
+printf("%d", array[i]);
+if (i != (size_t)high)
+printf(", ");
+}
+printf("\n");
 
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-
-    return -1; /* Value not found */
+if (array[mid] == value)
+return mid;
+else if (array[mid] < value)
+low = mid + 1;
+    else
+high = mid - 1;
+}
+return -1; /* Value not found */
 }
